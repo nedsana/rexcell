7543f4723e5c56b8206049c276348454988057d7
Nedelin Rusev <nedelin.rusev@gmail.com>
2026-04-24 21:58:17 +0300

    cfg_file: added option to list sheets

--- src/main.rs
+++ src/main.rs
@@ -10,32 +10,36 @@ struct Args {
     file: String,
 
     /// Column to search for duplicate text
-    #[arg(short = 's', long = "src_col", default_value = "C")]
+    #[arg(short = 's', long = "src-col", default_value = "C")]
     src_col: String,
 
     /// Column to write unique IDs into
-    #[arg(short = 'd', long = "dest_col", default_value = "B")]
+    #[arg(short = 'd', long = "dest-col", default_value = "B")]
     dest_col: String,
 
     /// Overwrite the input file instead of creating a new one
-    #[arg(short = 'i', long = "inplace")]
+    #[arg(short = 'i', long = "inplace", default_value = "false")]
     inplace: bool,
 
     /// reference table
-    #[arg(short = 'r', long = "ref")]
+    #[arg(short = 'r', long = "ref", default_value = "")]
     ref_table: String,
 
     /// reference table key column
-    #[arg(short = 'k', long = "key_col", default_value = "B")]
+    #[arg(short = 'k', long = "key-col", default_value = "B")]
     ref_col_key: String,
 
     /// reference table value column
-    #[arg(short = 'v', long = "value_col", default_value = "C")]
+    #[arg(short = 'v', long = "value-col", default_value = "C")]
     ref_col_value: String,
 
     /// update table
-    #[arg(short = 'u', long = "upd")]
+    #[arg(short = 'u', long = "upd", default_value = "")]
     upd_table: String,
+
+    /// list the worksheets in the file
+    #[arg(short = 'l', long = "list-sheets", default_value = "false")]
+    list_sheets: bool,
 }
 
 fn column_to_index(col: &str) -> u32 {
@@ -86,42 +90,56 @@ fn apply_key_value_data(sheet: &mut Worksheet, ref_map: &std::collections::HashM
     }
 }
 
+fn get_worksheet_names_list(book: & Spreadsheet) -> Vec<String> {
+    let sheets = book.get_sheet_collection();
+    sheets.iter().map(|s| s.get_name().to_string()).collect()
+}
+
+fn get_worksheet_names_string(book: & Spreadsheet) -> String {
+    get_worksheet_names_list(book).join(", ")
+}
+
 fn main() {
     let args = Args::parse();
 
     // Load the Excel file
     let path = std::path::Path::new(&args.file);
-    let mut book = reader::xlsx::read(path).expect("Can't read the file");
+    let mut book: Spreadsheet = reader::xlsx::read(path).expect("Can't read the file");
 
-    // Get the reference sheet
-    let rtbl = book.get_sheet_by_name(&args.ref_table).expect("The reference sheet is not found");
+    if args.list_sheets {
+        println!("{}", get_worksheet_names_string(&book)); 
+    }
+    else {
+        // Get the reference sheet
+        let rtbl = book.get_sheet_by_name(&args.ref_table).expect("The reference sheet is not found");
 
-    // Get the key-value entries from the reference table
-    use std::collections::HashMap;
-    let ref_map: HashMap<String, String> = get_ref_map(&rtbl, 
-                                                column_to_index(&args.ref_col_key), 
-                                                column_to_index(&args.ref_col_value));
-    
-    // Get the update sheet
-    let utbl = book.get_sheet_by_name_mut(&args.upd_table).expect("The update sheet is not found");
+        // Get the key-value entries from the reference table
+        use std::collections::HashMap;
+        let ref_map: HashMap<String, String> = get_ref_map(&rtbl, 
+                                                    column_to_index(&args.ref_col_key), 
+                                                    column_to_index(&args.ref_col_value));
+        
+        // Get the update sheet
+        let utbl = book.get_sheet_by_name_mut(&args.upd_table).expect("The update sheet is not found");
 
-    let applied = apply_key_value_data(
-        utbl,
-        &ref_map,
-        column_to_index(&args.src_col),
-        column_to_index(&args.dest_col),
-    )
-    .expect("No key-value mapping was applied");
+        let applied = apply_key_value_data(
+            utbl,
+            &ref_map,
+            column_to_index(&args.src_col),
+            column_to_index(&args.dest_col),
+        )
+        .expect("No key-value mapping was applied");
 
-    println!("Applied {} key-value mapping(s).", applied);
+        println!("Applied {} key-value mapping(s).", applied);
 
-    // Save changes
-    if args.inplace {
-        let _ = writer::xlsx::write(&book, path).expect("Unable to write the file");
-        println!("Done! The result is saved in '{}'", args.file);
-    } else {
-        let new_file = format!("{}_new.xlsx", args.file.trim_end_matches(".xlsx"));
-        let _ = writer::xlsx::write(&book, std::path::Path::new(&new_file)).expect("Unable to write the file");
-        println!("Done! The result is saved in '{}'", new_file);
+        // Save changes
+        if args.inplace {
+            let _ = writer::xlsx::write(&book, path).expect("Unable to write the file");
+            println!("Done! The result is saved in '{}'", args.file);
+        } else {
+            let new_file = format!("{}_new.xlsx", args.file.trim_end_matches(".xlsx"));
+            let _ = writer::xlsx::write(&book, std::path::Path::new(&new_file)).expect("Unable to write the file");
+            println!("Done! The result is saved in '{}'", new_file);
+        }        
     }
 }
