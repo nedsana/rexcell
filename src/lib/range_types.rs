ef6e7a78235c03946d1bc40072a1f39d91337a1a
Nedelin Rusev <nedelin.rusev@gmail.com>
2026-07-04 22:40:13 +0300

    merged-cells-ext: created standalone functions to unite implementation for RangeMultiline and RangeMultilineMut

--- src/lib/range_types.rs
+++ src/lib/range_types.rs
@@ -668,6 +668,204 @@ impl<'a> PartialEq for RangeMergedCellsMut<'a> {
     }
 }
 
+fn compare_multiline_range_impl<T>(
+    this: &T,
+    other: &dyn IRange,
+    strict: bool,
+    o_use_rows: Option<vec::Vec<u32>>,
+    o_use_cols: Option<vec::Vec<u32>>,
+    label: &str,
+) -> bool
+where
+    T: IRange,
+{
+    let range_a = this.get_range();
+    let range_b = other.get_range();
+
+    let brow_a = *range_a.get_coordinate_start_row().unwrap().get_num();
+    let erow_a = *range_a.get_coordinate_end_row().unwrap().get_num();
+    let bcol_a = *range_a.get_coordinate_start_col().unwrap().get_num();
+    let ecol_a = *range_a.get_coordinate_end_col().unwrap().get_num();
+
+    let brow_b = *range_b.get_coordinate_start_row().unwrap().get_num();
+    let erow_b = *range_b.get_coordinate_end_row().unwrap().get_num();
+    let bcol_b = *range_b.get_coordinate_start_col().unwrap().get_num();
+    let ecol_b = *range_b.get_coordinate_end_col().unwrap().get_num();
+
+    let rows_a = erow_a - brow_a + 1;
+    let cols_a = ecol_a - bcol_a + 1;
+    let rows_b = erow_b - brow_b + 1;
+    let cols_b = ecol_b - bcol_b + 1;
+
+    println!(
+        "[{}::compare_range] Range A:[{} Rows:{} Cols:{}] vs Range B:[{} Rows:{} Cols:{}]",
+        label,
+        range_ops::range_to_string(range_a),
+        rows_a,
+        cols_a,
+        range_ops::range_to_string(range_b),
+        rows_b,
+        cols_b
+    );
+
+    if strict && (rows_a != rows_b || cols_a != cols_b) {
+        println!(
+            "[{}::compare_range] Size missmatch! Range A:[{}, len:{}] != Range B:[{}, len:{}]",
+            label,
+            range_ops::range_to_string(range_a),
+            rows_a,
+            range_ops::range_to_string(range_b),
+            rows_b
+        );
+        return false;
+    }
+
+    let cols_a_offsets: Vec<u32> = (0..=(cols_a - 1)).collect();
+    let cols_b_offsets: Vec<u32> = (0..=(cols_b - 1)).collect();
+    let allowed_rows: Vec<u32> = o_use_rows.unwrap_or_default();
+    let allowed_cols: Vec<u32> = o_use_cols.unwrap_or_default();
+
+    let _str_allowed_rows = allowed_rows.iter().map(|r| range_ops::index_to_column(*r)).collect::<Vec<String>>().join(",");
+    let _str_allowed_cols = allowed_cols.iter().map(|c| range_ops::index_to_column(*c)).collect::<Vec<String>>().join(",");
+
+    let mut row_match = 0;
+    for row_num_a in brow_a..=erow_a {
+        if allowed_rows.len() > 0 && !allowed_rows.contains(&row_num_a) {
+            println!(
+                "[{}::compare_range] A{} is not in the allowed row list: {}!",
+                label,
+                row_num_a,
+                _str_allowed_rows
+            );
+            continue;
+        }
+
+        for row_num_b in brow_b..=erow_b {
+            if allowed_rows.len() > 0 && !allowed_rows.contains(&row_num_b) {
+                println!(
+                    "[{}::compare_range] B{} is not in the allowed row list: {}!",
+                    label,
+                    row_num_b,
+                    _str_allowed_rows
+                );
+                continue;
+            }
+
+            let mut col_match = 0;
+            for (col_a_offset, col_b_offset) in cols_a_offsets.iter().zip(cols_b_offsets.iter()) {
+                let col_num_a = bcol_a + col_a_offset;
+                let col_num_b = bcol_b + col_b_offset;
+
+                if allowed_cols.len() > 0 && !allowed_cols.contains(&col_num_a) && !allowed_cols.contains(&col_num_b) {
+                    println!(
+                        "[{}::compare_range] {}{} or {}{} is not in the allowed column list: {}!",
+                        label,
+                        range_ops::index_to_column(col_num_a),
+                        row_num_a,
+                        range_ops::index_to_column(col_num_b),
+                        row_num_b,
+                        _str_allowed_cols
+                    );
+                    col_match += 1;
+                    continue;
+                }
+
+                if this.compare_cell(other, col_num_a, row_num_a, col_num_b, row_num_b, strict) {
+                    col_match += 1;
+                }
+            }
+
+            println!(
+                "[{}::compare_range] Matching columns: {}/{} ! Returning {}",
+                label,
+                col_match,
+                cols_a,
+                col_match == cols_a
+            );
+
+            if col_match == cols_a {
+                row_match += 1;
+            }
+        }
+    }
+
+    println!(
+        "[{}::compare_range] Matching rows: {}/{} ! Returning {}",
+        label,
+        row_match,
+        rows_a,
+        row_match == rows_a
+    );
+
+    if !strict && row_match != rows_a {
+        return false;
+    }
+    true
+}
+
+fn compare_multiline_cell_impl<T>(
+    this: &T,
+    other: &dyn IRange,
+    col_a: u32,
+    row_a: u32,
+    col_b: u32,
+    row_b: u32,
+    strict: bool,
+    label: &str,
+) -> bool
+where
+    T: IRange,
+{
+    let sheet_a = this.get_sheet();
+    let sheet_b = other.get_sheet();
+
+    let mut r = false;
+    let cell_a_coord = (col_a, row_a);
+    let cell_b_coord = (col_b, row_b);
+    let val_a = sheet_a.get_cell_value(cell_a_coord).get_value();
+    let val_b = sheet_b.get_cell_value(cell_b_coord).get_value();
+
+    let cell_a_obj = sheet_a.get_cell(cell_a_coord);
+    let cell_b_obj = sheet_b.get_cell(cell_b_coord);
+    let rich_a = cell_a_obj.and_then(|c| c.get_cell_value().get_raw_value().get_rich_text());
+    let rich_b = cell_b_obj.and_then(|c| c.get_cell_value().get_raw_value().get_rich_text());
+
+    if rich_a != rich_b && strict {
+        println!(
+            "[{}::compare_cell] Rich text mismatch: {}:{} and {}:{}",
+            label,
+            range_ops::coords_to_str(col_a, row_a),
+            val_a,
+            range_ops::coords_to_str(col_b, row_b),
+            val_b
+        );
+        return r;
+    }
+
+    if range_ops::cmp_strs(&val_a, &val_b) {
+        println!(
+            "[{}::compare_cell] {}:{} equals {}:{}",
+            label,
+            range_ops::coords_to_str(col_a, row_a),
+            val_a,
+            range_ops::coords_to_str(col_b, row_b),
+            val_b
+        );
+        r = true;
+    } else {
+        println!(
+            "[{}::compare_cell] {}:{} differs {}:{}",
+            label,
+            range_ops::coords_to_str(col_a, row_a),
+            val_a,
+            range_ops::coords_to_str(col_b, row_b),
+            val_b
+        );
+        r = false;
+    }
+    r
+}
+
 // ----------------------  RangeMultiline ----------------------
 
 impl<'a> IRange for RangeMultiline<'a> {
@@ -685,131 +883,12 @@ impl<'a> IRange for RangeMultiline<'a> {
 
     fn compare_range(&self, other: &dyn IRange, strict: bool, o_use_rows: Option<vec::Vec<u32>>, o_use_cols: Option<vec::Vec<u32>>) -> bool 
     {
-        let range_a = self.get_range();
-        let range_b = other.get_range();
-
-        //Get the range numeric boundaries for range_a
-        let brow_a = *range_a.get_coordinate_start_row().unwrap().get_num();
-        let erow_a = *range_a.get_coordinate_end_row().unwrap().get_num();
-        let bcol_a = *range_a.get_coordinate_start_col().unwrap().get_num();
-        let ecol_a = *range_a.get_coordinate_end_col().unwrap().get_num();
-
-        //Get the range numeric boundaries for range_b
-        let brow_b = *range_b.get_coordinate_start_row().unwrap().get_num();
-        let erow_b = *range_b.get_coordinate_end_row().unwrap().get_num();
-        let bcol_b = *range_b.get_coordinate_start_col().unwrap().get_num();
-        let ecol_b = *range_b.get_coordinate_end_col().unwrap().get_num();
-
-        //Get the legths of the ranges (number of rows and columns)
-        let rows_a = erow_a - brow_a;
-        let cols_a = ecol_a - bcol_a;
-        let rows_b = erow_b - brow_b;
-        let cols_b = ecol_b - bcol_b;
-
-        //If the legths are different, the ranges cannot be the same
-        if strict && (rows_a != rows_b || cols_a != cols_b) 
-        {
-            println!("[RangeMultiline::compare_range] Size missmatch! Range A:[{}, len:{}] != Range B:[{}, len:{}]", 
-                    range_ops::range_to_string(range_a), rows_a, range_ops::range_to_string(range_b), rows_b);
-            return false;
-        }
-
-        let cols_a_offsets: Vec<u32> = (0..=cols_a).collect();
-        let cols_b_offsets: Vec<u32> = (0..=cols_b).collect();    
-        let allowed_rows: Vec<u32> = o_use_rows.unwrap_or_default();
-        let allowed_cols: Vec<u32> = o_use_cols.unwrap_or_default();
-
-        let _str_allowed_rows = allowed_rows.iter().map(|r| r.to_string()).collect::<Vec<String>>().join(",");
-        let _str_allowed_cols = allowed_cols.iter().map(|c| c.to_string()).collect::<Vec<String>>().join(",");
-
-        let mut row_match = 0;
-        let mut col_match = 0;
-
-        for row_num_a in brow_a..=erow_a 
-        {
-            if allowed_rows.len() > 0 && !allowed_rows.contains(&row_num_a)
-            {
-                // println!("[RangeMultiline::compare_range] A{} is not in the allowed row list: {}!", row_num_a, _str_allowed_rows);
-                continue; // skip this row if it's not in the allowed_rows list
-            }
-
-            for row_num_b in brow_b..=erow_b 
-            {
-                if allowed_rows.len() > 0 && !allowed_rows.contains(&row_num_b)
-                {
-                    // println!("[RangeMultiline::compare_range] B{} is not in the allowed row list: {}!", row_num_b, _str_allowed_rows);
-                    continue; // skip this row if it's not in the allowed_rows list
-                }
-
-                col_match = 0;
-                for (col_a_offset, col_b_offset) in cols_a_offsets.iter().zip(cols_b_offsets.iter()) 
-                {
-                    let col_num_a = bcol_a + col_a_offset;
-                    let col_num_b = bcol_b + col_b_offset;
-
-                    if allowed_cols.len() > 0 && !allowed_cols.contains(&col_num_a) && !allowed_cols.contains(&col_num_b) 
-                    {
-                        // println!("[RangeMultiline::compare_range] A{} or B{} is not in the allowed column list: {}!", col_num_a, col_num_b, _str_allowed_cols);
-                        col_match += 1;
-                        continue; // skip this column if it's not in the allowed_cols list
-                    }
-
-                    if self.compare_cell(other, col_num_a, row_num_a, col_num_b, row_num_b, strict)
-                    {
-                        col_match += 1;
-                    }
-                }
-            }
-
-            if col_match == cols_a
-            {
-                row_match += 1;
-            }
-        }
-
-        if !strict && row_match != rows_a 
-        {
-            return false;
-        }
-        true
+        compare_multiline_range_impl(self, other, strict, o_use_rows, o_use_cols, "RangeMultiline")
     }
 
     fn compare_cell(&self, other: &dyn IRange, col_a: u32, row_a: u32, col_b: u32, row_b: u32, strict: bool) -> bool 
     {
-        let sheet_a = self.get_sheet();
-        let sheet_b = other.get_sheet();
-
-        let mut r = false;
-        //Calculate the actual coordinates for sheet A and sheet B and get the text values of the two cells
-        let cell_a_coord = (col_a, row_a);
-        let cell_b_coord = (col_b, row_b);
-        let val_a = sheet_a.get_cell_value(cell_a_coord).get_value();
-        let val_b = sheet_b.get_cell_value(cell_b_coord).get_value();
-
-        //check if the cells have rich text and compare them if they do
-        let cell_a_obj = sheet_a.get_cell(cell_a_coord);
-        let cell_b_obj = sheet_b.get_cell(cell_b_coord);
-        let rich_a = cell_a_obj.and_then(|c| c.get_cell_value().get_raw_value().get_rich_text());
-        let rich_b = cell_b_obj.and_then(|c| c.get_cell_value().get_raw_value().get_rich_text());
-
-        if rich_a != rich_b && strict
-        {
-            println!("[RangeMultiline::compare_cell] Rich text mismatch: {}:{} and {}:{}", range_ops::coords_to_str(col_a, row_a), val_a, range_ops::coords_to_str(col_b, row_b), val_b);
-            return r;
-        }
-
-        // If there is any mismatch, immediately stop and return false
-        if range_ops::cmp_strs(&val_a, &val_b) 
-        {
-            println!("[RangeMultiline::compare_cell] {}:{} equals {}:{}", range_ops::coords_to_str(col_a, row_a), val_a, range_ops::coords_to_str(col_b, row_b), val_b);
-            r = true;
-        }
-        else 
-        {
-            println!("[RangeMultiline::compare_cell] {}:{} differs {}:{}", range_ops::coords_to_str(col_a, row_a), val_a, range_ops::coords_to_str(col_b, row_b), val_b); 
-            r = false;
-        }
-        r
+        compare_multiline_cell_impl(self, other, col_a, row_a, col_b, row_b, strict, "RangeMultiline")
     }
 }
 
@@ -828,135 +907,12 @@ impl<'a> IRange for RangeMultilineMut<'a> {
 
     fn compare_range(&self, other: &dyn IRange, strict: bool, o_use_rows: Option<vec::Vec<u32>>, o_use_cols: Option<vec::Vec<u32>>) -> bool 
     {
-        let range_a = self.get_range();
-        let range_b = other.get_range();
-
-        //Get the range numeric boundaries for range_a
-        let brow_a = *range_a.get_coordinate_start_row().unwrap().get_num();
-        let erow_a = *range_a.get_coordinate_end_row().unwrap().get_num();
-        let bcol_a = *range_a.get_coordinate_start_col().unwrap().get_num();
-        let ecol_a = *range_a.get_coordinate_end_col().unwrap().get_num();
-
-        //Get the range numeric boundaries for range_b
-        let brow_b = *range_b.get_coordinate_start_row().unwrap().get_num();
-        let erow_b = *range_b.get_coordinate_end_row().unwrap().get_num();
-        let bcol_b = *range_b.get_coordinate_start_col().unwrap().get_num();
-        let ecol_b = *range_b.get_coordinate_end_col().unwrap().get_num();
-
-        //Get the legths of the ranges (number of rows and columns)
-        let rows_a = erow_a - brow_a + 1;
-        let cols_a = ecol_a - bcol_a + 1;
-        let rows_b = erow_b - brow_b + 1;
-        let cols_b = ecol_b - bcol_b + 1;
-
-        println!("[RangeMultilineMut::compare_range] Range A:[{} Rows:{} Cols:{}] vs Range B:[{} Rows:{} Cols:{}]", 
-                range_ops::range_to_string(range_a), rows_a, cols_a, range_ops::range_to_string(range_b), rows_b, cols_b);
-
-        //If the legths are different, the ranges cannot be the same
-        if strict && (rows_a != rows_b || cols_a != cols_b) 
-        {
-            println!("[RangeMultilineMut::compare_range] Size missmatch! Range A:[{}, len:{}] != Range B:[{}, len:{}]", 
-                    range_ops::range_to_string(range_a), rows_a, range_ops::range_to_string(range_b), rows_b);
-            return false;
-        }
-
-        let cols_a_offsets: Vec<u32> = (0..=(cols_a-1)).collect();
-        let cols_b_offsets: Vec<u32> = (0..=(cols_b-1)).collect();
-        let allowed_rows: Vec<u32> = o_use_rows.unwrap_or_default();
-        let allowed_cols: Vec<u32> = o_use_cols.unwrap_or_default();
-
-        let _str_allowed_rows = allowed_rows.iter().map(|r| range_ops::index_to_column(*r)).collect::<Vec<String>>().join(",");
-        let _str_allowed_cols = allowed_cols.iter().map(|c| range_ops::index_to_column(*c)).collect::<Vec<String>>().join(",");
-
-        let mut row_match = 0;
-        for row_num_a in brow_a..=erow_a 
-        {
-            if allowed_rows.len() > 0 && !allowed_rows.contains(&row_num_a)
-            {
-                // println!("[RangeMultilineMut::compare_range] A{} is not in the allowed row list: {}!", row_num_a, _str_allowed_rows);
-                continue; // skip this row if it's not in the allowed_rows list
-            }
-
-            for row_num_b in brow_b..=erow_b 
-            {
-                if allowed_rows.len() > 0 && !allowed_rows.contains(&row_num_b)
-                {
-                    // println!("[RangeMultilineMut::compare_range] B{} is not in the allowed row list: {}!", row_num_b, _str_allowed_rows);
-                    continue; // skip this row if it's not in the allowed_rows list
-                }
-
-                let mut col_match = 0;
-                for (col_a_offset, col_b_offset) in cols_a_offsets.iter().zip(cols_b_offsets.iter()) 
-                {
-                    let col_num_a = bcol_a + col_a_offset;
-                    let col_num_b = bcol_b + col_b_offset;
-
-                    if allowed_cols.len() > 0 && !allowed_cols.contains(&col_num_a) && !allowed_cols.contains(&col_num_b) 
-                    {
-                        col_match += 1;
-                        // println!("[RangeMultilineMut::compare_range] {}{} or {}{} is not in the allowed column list: {}! col_match:{}", 
-                        //     range_ops::index_to_column(col_num_a), row_num_a, range_ops::index_to_column(col_num_b), row_num_b, _str_allowed_cols, col_match);
-                        continue; // skip this column if it's not in the allowed_cols list
-                    }
-
-                    if self.compare_cell(other, col_num_a, row_num_a, col_num_b, row_num_b, strict)
-                    {
-                        col_match += 1;
-                    }
-                }
-
-                println!("[RangeMultilineMut::compare_range] Matching columns: {}/{} ! Returning {}", col_match, cols_a, col_match == cols_a);
-                if col_match == cols_a
-                {
-                    row_match += 1;
-                }
-            }
-        }
-
-        println!("[RangeMultilineMut::compare_range] Matching rows: {}/{} ! Returning {}", row_match, rows_a, row_match == rows_a);
-        if !strict && row_match != rows_a 
-        {
-            return false;
-        }
-        true
+        compare_multiline_range_impl(self, other, strict, o_use_rows, o_use_cols, "RangeMultilineMut")
     }
 
     fn compare_cell(&self, other: &dyn IRange, col_a: u32, row_a: u32, col_b: u32, row_b: u32, strict: bool) -> bool 
     {
-        let sheet_a = self.get_sheet();
-        let sheet_b = other.get_sheet();
-
-        let mut r = false;
-        //Calculate the actual coordinates for sheet A and sheet B and get the text values of the two cells
-        let cell_a_coord = (col_a, row_a);
-        let cell_b_coord = (col_b, row_b);
-        let val_a = sheet_a.get_cell_value(cell_a_coord).get_value();
-        let val_b = sheet_b.get_cell_value(cell_b_coord).get_value();
-
-        //check if the cells have rich text and compare them if they do
-        let cell_a_obj = sheet_a.get_cell(cell_a_coord);
-        let cell_b_obj = sheet_b.get_cell(cell_b_coord);
-        let rich_a = cell_a_obj.and_then(|c| c.get_cell_value().get_raw_value().get_rich_text());
-        let rich_b = cell_b_obj.and_then(|c| c.get_cell_value().get_raw_value().get_rich_text());
-
-        if rich_a != rich_b && strict
-        {
-            println!("[RangeMultilineMut::compare_cell] Rich text mismatch: {}:{} and {}:{}", range_ops::coords_to_str(col_a, row_a), val_a, range_ops::coords_to_str(col_b, row_b), val_b);
-            return r;
-        }
-
-        // If there is any mismatch, immediately stop and return false
-        if range_ops::cmp_strs(&val_a, &val_b) 
-        {
-            println!("[RangeMultilineMut::compare_cell] {}:{} equals {}:{}", range_ops::coords_to_str(col_a, row_a), val_a, range_ops::coords_to_str(col_b, row_b), val_b);
-            r = true;
-        }
-        else 
-        {
-            println!("[RangeMultilineMut::compare_cell] {}:{} differs {}:{}", range_ops::coords_to_str(col_a, row_a), val_a, range_ops::coords_to_str(col_b, row_b), val_b); 
-            r = false;
-        }
-        r
+        compare_multiline_cell_impl(self, other, col_a, row_a, col_b, row_b, strict, "RangeMultilineMut")
     }
 }
 
